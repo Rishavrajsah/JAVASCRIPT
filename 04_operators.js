c55c47ed36ea0f139e74ea2aeb871b9ea2b606bd
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-19 14:33:41 +0530

    operators

---
+++ 04_operators.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 2;
+console.log("a+b =",a+b);   
+console.log("a-b =",a-b); 
+console.log("a*b =",a*b); 
+console.log("a/b =",a/b);     
+console.log("a%b =",a%b);
+console.log("a**b =",a**b); 
+console.log("a++ =",a++);
+console.log("a =",a);    
+// ternary operator
+let age= 25   
+let result = age>=18?"adult":"not adult";
+console.log(result);
