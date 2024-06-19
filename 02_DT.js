f36812e7b4fe5af8480687263cc4f4cca0752cbb
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-19 11:55:17 +0530

    conversion

---
+++ 02_DT.js
@@ -0,0 +1,13 @@
+"use strict"  // treat all js code newer version 
+// alert(3+4);  we are using node js not browser
+
+
+// all 7 primitive datatypes
+                     //type of
+let name = "rishav"; // string
+let roll = 1; // number 
+let isFollow = true; // boolean
+let a;    // undefined
+let b = null; // object 
+let c = BigInt(123); // bigint 
+let d = Symbol("hello"); // symbol
