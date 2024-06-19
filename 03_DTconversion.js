d900c90e14e2084863319e35b63bf613cffef3de
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-19 12:54:43 +0530

    update

---
+++ 03_DTconversion.js
@@ -0,0 +1,30 @@
+// string to number
+let score = "44";
+console.log(typeof score);
+let a = Number(score);
+console.log(typeof a);
+console.log(a);
+/*
+"33" => 33
+"33abs" => NaN
+null => 0
+undefined => NaN
+true => 1
+false => 0
+*/
+// number to boolean
+let isFollow = 1;
+let boolisFollow = Boolean(isFollow);
+console.log(boolisFollow);
+/*
+1 => true
+0 => false
+empty string "" => false
+"rishav" => true
+*/
+
+// number to String
+let abc = 69;
+let stringabc = String(abc);
+console.log(stringabc);
+console.log(typeof stringabc);
