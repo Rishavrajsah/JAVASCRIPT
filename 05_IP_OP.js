68dea6c84914edb27effabc0c89552668412d1eb
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-19 14:51:56 +0530

    update

---
+++ 05_IP_OP.js
@@ -0,0 +1,9 @@
+// alert("hello");
+// let fname =prompt("Write your name");
+// console.log("your name is "+ fname);
+let a = prompt("enter a number");
+if (a%5===0) {
+    console.log(a,"is multiple of 5");
+}else{
+    console.log(a + " is not multiple of 5");
+}
