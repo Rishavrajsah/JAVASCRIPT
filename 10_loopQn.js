4a3f8dd36f152bd8cbfb1e7eea64e3649fb0a9ac
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-20 22:33:03 +0530

    update

---
+++ 10_loopQn.js
@@ -0,0 +1,8 @@
+let number = prompt("guess number between 1-10");
+let a =5;
+while(number!=a){
+    console.log("guess right number");
+    number = prompt("guess number between 1-10")
+}
+console.log("your number is 5");
+console.log(typeof number);
