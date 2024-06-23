fd9619410f185f68fe9a99423a51bc943dc31490
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-23 20:16:17 +0530

    update

---
+++ 12_memory.js
@@ -0,0 +1,4 @@
+//datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
