743c42e933a80a5d5a875a196b35b53527e9bc4f
Rishavrajsah <rishavrajsah277@gmail.com>
2024-06-23 23:04:52 +0530

    update

---
+++ 14_Dates.js
@@ -0,0 +1,9 @@
+// Dates
+let mydate = new Date();
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toJSON());
+console.log(mydate.toTimeString());
