d264c9c93930facbec10732817cd4f9901b9a35a
Rishav <rishavrajsah277@gmail.com>
2025-02-01 11:52:56 +0530

    add new files

---
+++ 22_dom1.js
@@ -0,0 +1,20 @@
+// DoM  - document object model
+console.log(window);
+// console.log(window.document); or console.log(document);
+console.dir(document)
+
+console.log(document.baseURI); // provide base url link
+console.log(document.links); // provide all links on web page 
+console.log(document.links[2]); // provide particular link number
+
+
+document.getElementById("name")
+// manipulation
+
+document.getElementById("name").innerHTML = "<h1>new name</h1>"
+
+
+
+
+
+
---
+++ 23_dom2.js
@@ -0,0 +1,37 @@
+document.getElementById('title')
+document.getElementById('title').id  // op- title
+document.getElementById('title').class // op- undefine
+document.getElementById('title').className // op - "heading"
+// or 
+document.getElementById('title').getAttribute('id')
+document.getElementById('title').getAttribute('class')
+// set attribute   overwrite 
+document.getElementById('title').setAttribute('class','test')
+
+// store vale in a variable 
+const title = document.getElementById('title');
+// manipulation
+title.style.backgroundColor = 'green'
+
+// get content 
+title.textContent    // op- dom learning on chai or code         //  give hidden content     "DOM learning on Chai aur code test text"
+title.innerHTML    // op- dom learning on chai or code   // give all values including html  "DOM learning on Chai aur code <span style="display: none;">test text</span>"
+title.innerText    // op- dom learning on chai or code    // didn't give hidden content      "DOM learning on Chai aur code"
+
+
+// querySlector
+document.querySelector('h2') // give first h2 heading
+document.querySelector('#title')  // for id search
+document.querySelector('.heading') // for class search
+
+// querySelectorAll
+/*<ul>
+            <li class="list-item">one</li>
+            <li class="list-item">two</li>
+            <li class="list-item">three</li>
+            <li class="list-item">four</li>
+</ul> 
+*/
+const tempLiList = document.querySelectorAll('li')  // select all iteam in list not only first element
+console.log(tempLiList); // prove Nodelist not an array
+
